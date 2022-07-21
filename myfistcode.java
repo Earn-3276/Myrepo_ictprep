b9ad1c3592e4d152c4c24bd7f19fd8b8504ea3db
Netsara srisai <109579673+Earn-3276@users.noreply.github.com>
2022-07-21 16:50:23 +0700

    Update myfistcode.java

--- myfistcode.java
+++ myfistcode.java
@@ -1,4 +1,8 @@
- This is my first code in java
+// This is my first code in java
 
-
+class HelloWorld {
+    public static void main(String[] ards) {
+         Systen.out.println("Hello'World!");
+    }
+}
  
